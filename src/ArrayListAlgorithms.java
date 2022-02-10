9b0860010df0137b0ed9f89cee02675b1e7240aa
katrinal13 <katrinal13@nycstudents.net>
2022-02-10 02:25:24 -0500

    updated U7T4Lab

--- src/ArrayListAlgorithms.java
+++ src/ArrayListAlgorithms.java
@@ -185,12 +185,160 @@ public class ArrayListAlgorithms
         {
             for (int j = 0; j < intList.size(); j++)
             {
-                if (intList.get(i) == intList.get(j))
+                if (intList.get(i) == intList.get(j) && i != j)
                 {
-                    intList.remove(i);
-                    i--;
+                    intList.remove(j);
+                    j--;
                 }
             }
         }
+    }
+
+    /** Adds an uppercase version of each string directly AFTER the string
+     *  in wordList; for example, if wordList is ["hello", "my", "best", "friend"]
+     *  this this method modifies wordList to be:
+     *  ["hello", "HELLO", "my", "MY", "best", "BEST", "friend", "FRIEND"]
+     *
+     *  Assume all words have lowercase letters originally
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITION: wordList.size() > 0
+     *
+     *  @param wordList  arraylist of Strings
+     */
+    public static void duplicateUpperAfter(ArrayList<String> wordList)
+    {
+        for (int i = 0; i < wordList.size(); i += 2)
+        {
+            wordList.add(i + 1, wordList.get(i).toUpperCase());
+        }
+    }
+
+    /** Appends an uppercase version of each string to the END of of wordList;
+     *  the uppercase versions appear in the same order as the lowercase versions
+     *  for example, if wordList is ["hello", "my", "best", "friend"]
+     *  this this method modifies wordList to be:
+     *  ["hello", "my", "best", "friend", "HELLO", "MY", "BEST", "FRIEND"]
+     *
+     *  Assume all words have lowercase letters originally
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITION: wordList.size() > 0
+     *
+     *  @param wordList  arraylist of Strings
+     */
+    public static void duplicateUpperEnd(ArrayList<String> wordList)
+    {
+        int size = wordList.size();
+        for (int i = 0; i < size; i++)
+        {
+            wordList.add(wordList.get(i).toUpperCase());
+        }
+    }
+
+    /** Returns an arraylist of Strings that represents the input sentence parsed
+     *  into separate words (using a space: " " as the delimiter) and REVERSED
+     *  For example, if sentence = "This is my sentence!"
+     *  this method return [sentence!, my, is, This]
+     *
+     *  PRECONDITION: sentence does not end with a space
+     *
+     *  @param sentence  the input String that represents one or more words
+    separated by spaces
+     *  @return  new arraylist of Strings containing the words of sentence reversed
+     */
+    public static ArrayList<String> parseWordsAndReverse(String sentence)
+    {
+        ArrayList<String> parsed = new ArrayList<String>();
+        String space = " ";
+        int j = 0;
+        for (int i = 0; i < sentence.length(); i++)
+        {
+            if (sentence.substring(i, i + 1).equals(space))
+            {
+                parsed.add(0, sentence.substring(j, i));
+                j = i + 1;
+            }
+        }
+        parsed.add(0, sentence.substring(j, sentence.length()));
+        return parsed;
+    }
+
+    /** Removes all words from wordList that begin with "b" and inserts them at the
+     *  front of wordList; all "b" words that are moved should appear in the same order
+     *  in the modified arrayList as they did before being moved
+     *
+     *  For example, this method will take a wordList:
+     *  ["apple", "banana", "cherry", "donut", "bagel", "danish", "berry", "baguette", "soda"]
+     *  and modify it to
+     *  ["banana", "bagel", "berry", "baguette", "apple", "cherry", "donut", "danish", "soda"]
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITIONS: wordList.size() > 0, all strings in wordList have at least one character
+     *
+     *  @param wordList  arraylist of words
+     */
+    public static void moveBWords(ArrayList<String> wordList)
+    {
+        int indexNext = 0;
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            if (wordList.get(i).substring(0, 1).equals("b"))
+            {
+                String removed = wordList.remove(i);
+                wordList.add(indexNext, removed);
+                indexNext++;
+            }
+        }
+    }
+
+    /** Returns arraylist of Integers that contain all mode(s) of the int array numList.
+     *  If elements in numList all appear exactly once, there is no mode, and this method
+     *  should return an empty list
+     *
+     *  For example, if numList is: [1, 2, 3, 2, 4, 5, 5, 6],
+     *  then numList contains two modes: 2, 5
+     *  and this method returns an arraylist containing 2 and 5 (in any order)
+     *  If numList is: [1, 2, 3, 2, 4, 5, 5, 6, 6, 7, 6],
+     *  then numList contains one mode: 6
+     *  and this method returns an arrayList containing 6
+     *  If numList is: [1, 2, 3, 4, 5, 6], then numList contains no mode
+     *  and this method returns an empty arrayList: []
+     *  If numList is: [2, 2, 2, 3, 3, 3, 4, 4, 4],
+     *  then numList contains three modes: 2, 3, 4
+     *  and this method returns an arrayList containing 2, 3, and 4 (in any order)
+     *
+     *  Does NOT mutate (modify) elements in numList
+     *  PRECONDITIONS: numList.length > 0
+     *
+     *  @param numList  numList of ints
+     */
+    public static ArrayList<Integer> modes(int[] numList)
+    {
+        ArrayList<Integer> modes = new ArrayList<Integer>();
+        int maxAppearances = 1;
+        for (int i = 0; i < numList.length; i++)
+        {
+            int currentAppearances = 1;
+            for (int j = i + 1; j < numList.length; j++)
+            {
+                if (numList[i] == numList[j])
+                {
+                    currentAppearances++;
+                }
+            }
+
+            if (currentAppearances > maxAppearances)
+            {
+                maxAppearances = currentAppearances;
+                modes.clear();
+                modes.add(numList[i]);
+            }
+            else if (currentAppearances == maxAppearances && currentAppearances != 1)
+            {
+                modes.add(numList[i]);
+            }
+        }
+        return modes;
     }
 }
